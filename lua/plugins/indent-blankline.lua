aa22a7ebca69d371a4a4e00c82a68728f8128bc1
전희원 <xt.wish1@gmail.com>
2025-10-28 16:59:29 +0900

    Refactor indent-blankline configuration
    Removed highlight configuration and disabled scope.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,12 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
   opts = {
     indent = {
       char = "▏",
-      highlight = {
-        "IndentBlanklineChar",
-      },
+    },
+    scope = {
+      enabled = false,
     },
   },
-  config = function(_, opts)
-    require("ibl").setup(opts)
-
-    -- 색상을 아주 연하게 설정
-    vim.cmd [[highlight IndentBlanklineChar guifg=#2a2a2a gui=nocombine]]
-  end
 }
