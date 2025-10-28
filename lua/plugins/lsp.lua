ff4ba4bbd4a32b37aa311b090f58b6dd2da859e9
전희원 <xt.wish1@gmail.com>
2025-10-28 14:33:37 +0900

    Update Lua LSP settings and configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -209,16 +209,25 @@ return {
       --
 
       lua_ls = {
-        -- cmd = { ... },
-        -- filetypes = { ... },
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+              }
+            },
+            diagnostics = {
+              globals = { 'vim' },
+              disable = { 'missing-fields' },
+            },
+            format = {
+              enable = false,
+            },
           },
         },
       },
@@ -242,20 +251,11 @@ return {
       'stylua', -- Used to format Lua code
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    -- Handle LSP setups
+    -- See :help vim.lsp.enable
+    for server, config in pairs(servers) do
+      vim.lsp.config(server, config)
+      vim.lsp.enable(server)
+    end
   end,
 }
