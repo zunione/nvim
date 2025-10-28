5ea8a1c9e54b6a1fa3c1227ec8381ca7dca851b6
전희원 <xt.wish1@gmail.com>
2025-10-28 16:59:06 +0900

    Update and rename colortheme.lua to colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,43 @@
+return {
+  'Mofiqul/vscode.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local c = require('vscode.colors').get_colors()
+    require('vscode').setup({
+      -- Alternatively set style in setup
+      -- style = 'light'
+
+      -- Enable transparent background
+      transparent = false,
+
+      -- Enable italic comment
+      italic_comments = true,
+
+      -- Enable italic inlay type hints
+      italic_inlayhints = true,
+
+      -- Underline `@markup.link.*` variants
+      underline_links = true,
+
+      -- Disable nvim-tree background color
+      disable_nvimtree_bg = true,
+
+      -- Apply theme colors to terminal
+      terminal_colors = true,
+
+      -- Override colors (see ./lua/vscode/colors.lua)
+      color_overrides = {
+        vscLineNumber = '#FFFFFF',
+      },
+
+      -- Override highlight groups (see ./lua/vscode/theme.lua)
+      group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+      }
+    })
+    vim.cmd.colorscheme('vscode')
+  end
+}
