c1145486ee38a27ce802b856e753ad98b1d85291
전희원 <xt.wish1@gmail.com>
2025-11-10 15:19:29 +0900

    Enhance options with autocommands and cursor line settings
    Added autocommands for file type specific indentation and improved cursor line highlighting.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,65 +1,50 @@
--- Enable mouse mode (default: '')
-vim.o.mouse = 'a'
+--[[
+-   NOTE      - keymaps.lua
+-   Author    - zunione
+-
+-   Created   - 2025.11.05
+-   Github    - https://github.com/zunione
+-   Contact   - xt.wish1@gmail.com
+]]
 
--- Make line numbers default (default: false)
-vim.wo.number = true
--- Set relative numbered lines (default: false)
-vim.o.relativenumber = true
 
--- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
 
--- Sync clipboard between OS and Neovim (default: '')
-vim.o.clipboard = 'unnamedplus'
+-- ========================= --
+-- =     UI & DISPLAY      = --
+-- ========================= --
+vim.o.mouse = 'a'                   -- Enable mouse mode (default: '')
+vim.wo.number = true                -- Make line numbers default (default: false)
+vim.o.relativenumber = true         -- Set relative numbered lines (default: false)
 
--- Display lines as one long line (default: true)
-vim.o.wrap = false
--- Companion to wrap, don't split words (default: false)
-vim.o.linebreak = true
+vim.o.cursorline = true                       -- Highlight current line
+vim.api.nvim_create_autocmd("ColorScheme", {  -- Set colors for cursorline highlight
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2d2d2d" })                -- Set the background color of the CursorLine
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffaa", bold = true }) -- Set the foreground color of the CursorLineNr
+  end,
+})
 
--- Smart case (default: false)
-vim.o.smartcase = true
--- Case-insensitive searching UNLESS \C or capital in search (default: false)
-vim.o.ignorecase = true
+vim.o.signcolumn = 'yes'            -- Keep signcolumn on by default
+vim.o.wrap = false                  -- Display lines as one long line (default: true)
+vim.o.linebreak = true              -- Companion to wrap, don't split words (default: false)
+vim.o.showtabline = 1               -- Show tab line if there are at least two tabs
+vim.opt.termguicolors = true        -- set termguicolors to enable highlight groups
 
--- make indenting smarter again
-vim.o.smartindent = true
--- Copy indent from current line when starting new one (default: true)
-vim.o.autoindent = true
--- Enable break indent
-vim.o.breakindent = true
 
--- The number of spaces inserted for each indentation (default: 8)
-vim.o.shiftwidth = 4
--- Insert N spaces for a tab (default: 8)
-vim.o.tabstop = 4
--- Number of spaces that a tab counts for while performing editing operations
-vim.o.softtabstop = 4
--- Convert tabs to spaces (default: false)
-vim.o.expandtab = true
 
--- Show tab line if there are at least two tabs
-vim.o.showtabline = 1
+-- ========================= --
+-- =        INDENT         = --
+-- ========================= --
+vim.o.smartindent = true            -- make indenting smarter again
+vim.o.autoindent = true             -- Copy indent from current line when starting new one (default: true)
+vim.o.breakindent = true            -- Enable break indent
 
--- Save undo history
-vim.o.undofile = true
+vim.o.shiftwidth = 4                -- The number of spaces inserted for each indentation (default: 8)
+vim.o.tabstop = 4                   -- Insert N spaces for a tab (default: 8)
+vim.o.softtabstop = 4               -- Number of spaces that a tab counts for while performing editing operations
 
--- the encoding written to a file
-vim.o.fileencoding = 'utf-8'
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- don't insert the current comment leader automatically
-vim.opt.formatoptions:remove { 'c', 'r', 'o' }
-
--- separate vim plugins from neovim
-vim.opt.runtimepath:remove '/usr/share/vim/vimfiles'
-
--- allow backspace on
-vim.o.backspace = 'eol,start'
-
--- 파일 타입별 인덴트 설정
+-- File type specific indent settings
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {"lua", "dart", "javascript", "typescript", "typescriptreact", "javascriptreact"},
   callback = function()
@@ -68,3 +53,30 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+
+
+-- ========================= --
+-- =        EDITING        = --
+-- ========================= --
+vim.o.expandtab = true                          -- Convert tabs to spaces (default: false)
+vim.o.backspace = 'indent,eol,start'            -- allow backspace on
+vim.opt.formatoptions:remove { 'c', 'r', 'o' }  -- don't insert the current comment leader automatically
+
+
+
+-- ========================= --
+-- =         SEARCH        = --
+-- ========================= --
+vim.o.smartcase = true              -- Smart case (default: false)
+vim.o.ignorecase = true             -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+
+
+
+-- ========================= --
+-- =    SYSTEM & FILES     = --
+-- ========================= --
+vim.o.clipboard = 'unnamedplus'                       -- Sync clipboard between OS and Neovim (default: '')
+vim.o.undofile = true                                 -- Save undo history
+vim.o.fileencoding = 'utf-8'                          -- the encoding written to a file
+vim.opt.runtimepath:remove '/usr/share/vim/vimfiles'  -- separate vim plugins from neovim
