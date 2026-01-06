7bff9edc6b2d98922486b832f4f1955bc296ee6d
전희원 <xt.wish1@gmail.com>
2026-01-06 16:13:58 +0900

    Add toggleterm.nvim plugin configuration

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    -- 'tt'를 눌렀을 때 터미널이 토글되도록 설정
+    open_mapping = '<C-T>',
+    -- (선택 사항) 터미널 내부에서도 'tt'로 토글하려면
+    terminal_mappings = true
+  }
+}
